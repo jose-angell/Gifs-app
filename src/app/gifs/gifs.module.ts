4da9b99eafe817936b9646e508f76df68a920f07
jose Angel <josegallardo.cor@gmail.com>
2024-01-21 18:37:29 -0600

    shearchBoxComponent Agregado

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomePageComponent } from './pages/home/home-page.component';
+import { SerarchBoxComponent } from './components/search-box/shearch-box.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SerarchBoxComponent
   ],
   imports: [
     CommonModule
