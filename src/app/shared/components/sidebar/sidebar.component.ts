ef8631358e31f80f80594dd3cff5c33c71c4d86a
jose Angel <josegallardo.cor@gmail.com>
2024-01-26 18:37:04 -0600

    ajustes en sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -4,7 +4,7 @@ import { GifsService } from '../../../gifs/services/gifs.service';
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-
+  styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
